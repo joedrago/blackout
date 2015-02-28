f62d97daab212a8c950413906aa7aa1b9f7f3a8a
Joe Drago <joedrago@gmail.com>
2015-02-28 01:22:26 -0800

    stub

---
+++ src/com/jdrago/blackout/BlackoutActivity.java
@@ -0,0 +1,15 @@
+package com.jdrago.blackout;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BlackoutActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
