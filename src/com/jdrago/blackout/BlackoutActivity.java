ae5021bf96665b689bd7fff5afb349dfe3a9e62a
Joe Drago <joedrago@gmail.com>
2015-02-28 16:38:36 -0800

    enable "immersive" mode

--- src/com/jdrago/blackout/BlackoutActivity.java
+++ src/com/jdrago/blackout/BlackoutActivity.java
@@ -3,6 +3,7 @@ package com.jdrago.blackout;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import com.jdrago.blackout.bridge.*;
 
@@ -18,15 +19,39 @@ class BlackoutGame implements NativeApp
 
 public class BlackoutActivity extends Activity
 {
+    private static final String TAG = "Blackout";
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        immerse();
 
         BlackoutGame game = new BlackoutGame();
         Script script = new Script();
         script.startup(game);
+    }
+
+    @Override
+    protected void onResume()
+    {
+        Log.d(TAG, "Resuming the App");
+
+        super.onResume();
+        immerse();
+    }
+
+    void immerse()
+    {
+        this.getWindow().getDecorView().setSystemUiVisibility(
+                      View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    | View.INVISIBLE);
     }
 }
